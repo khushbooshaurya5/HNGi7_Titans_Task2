5cd3e0d0be022ac70114e158f512ce7cc5c47d39
Khushboo kumari <53648047+khushbooshaurya5@users.noreply.github.com>
2020-06-04 02:21:12 +0530

    Update khushbookumari.js

--- scripts/khushbookumari.js
+++ scripts/khushbookumari.js
@@ -1,1 +1,1 @@
-console.log("Hello World, this is Khushboo kumari  with HNGi7 ID: HNG 04622 and khushbooshaurya@gmail.com using javascript for stage 2 task."); 
+console.log("Hello World, this is Khushboo kumari  with HNGi7 ID: HNG 04622 and Email: khushbooshaurya@gmail.com using javascript for stage 2 task."); 
